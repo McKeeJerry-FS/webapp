b54987553e7ca2462e88280377724f9026dacb1c
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-03-19 21:22:47 -0400

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { createServer } from "http";
+import { handler } from "./handler";
+
+const port = 3000;
+const server = createServer(handler);
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
